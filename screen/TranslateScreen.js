83d1d29f8828d8521673546653d453fe871a39af
yoonseo1 <dbstj0319@naver.com>
2021-05-09 15:36:00 +0900

    feature: add translate button

--- screen/TranslateScreen.js
+++ screen/TranslateScreen.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
-import {Text,View,} from 'react-native';
+import {Text,View,TouchableOpacity, StyleSheet,Alert } from 'react-native';
 
 export default function TranslateScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity
+        style = {styles.button}
+        onPress={()=>Alert.alert('업로드 하시겠습니까?')}
+      >
+        <Text>업로드</Text>
+      </TouchableOpacity>
       <Text>Translate!</Text>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  button: {
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 50,
+    width: 100,
+    backgroundColor: 'white',
+    borderColor: 'black',
+    alignItems: 'center',
+    paddingVertical: 15
+  }
+})
